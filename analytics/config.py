24c2b7a1472f9cd42a61e837c07149176a4c79e2
Terence Manuel <tmudacitycourse@gmail.com>
2025-04-04 23:53:42 +0000

    Update docker

--- analytics/config.py
+++ analytics/config.py
@@ -4,11 +4,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["DB_USER"]
-db_password = os.environ["DB_PASSWORD"]
+db_username =  os.environ.get("DB_USER", "tm-udacity-coworking-db-user")
+db_password = os.environ.get("DB_PASSWORD", "tm-udacity-coworking-db-uR")
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_port = os.environ.get("DB_PORT", "5432")
-db_name = os.environ.get("DB_NAME", "postgres")
+db_name = os.environ.get("DB_NAME", "tm-udacity-coworking-db")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
